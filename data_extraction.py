3b9eb074dcb565a5bb232a7a1c5cd2d4374e773c
Thilakshan <thilakshanbalasubramaniam9@gmail.com>
2023-05-19 00:11:57 +0100

    Extracts database table to a pandas dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,13 @@
+from database_utils import DatabaseConnector
+import pandas as pd
 class DataExtractor:
     def __init__(self):
         pass
+    def read_rds_table(self, connect, table_name):
+            engine = connect.init_db_engine()
+            users = pd.read_sql_query(f'''SELECT * FROM {table_name}''', engine).set_index('index')
+            return users
+    
+data_1 = DataExtractor()
+data_2 = DatabaseConnector()
+print(data_1.read_rds_table(data_2, 'legacy_users'))
