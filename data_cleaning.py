10b6081d9fececa6255a1e4614b1e4af047fd737
Thilakshan <thilakshanbalasubramaniam9@gmail.com>
2023-05-21 16:43:20 +0100

    Manages to convert all data time formats to the same format

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,4 @@
-
 import pandas as pd
-
 class DataCleaning:
     def __init__(self):
         pass
@@ -9,12 +7,21 @@ class DataCleaning:
         print(f"Total null values in the dataframe are : {users.isna().sum().sum()}")
         # Printing the data types to see if they are of the right type
         print(users.dtypes)
+        users['date_of_birth'].to_string('dob_frame.txt')
         users['country'] = users['country'].astype('category')
         users['country_code'] = users['country_code'].astype('category')
-        users['date_of_birth'] = pd.to_datetime(users['date_of_birth'], errors='coerce')
-        users = users.dropna(subset=['date_of_birth'])
-        users['join_date'] = pd.to_datetime(users['join_date'], errors='coerce')
-        users = users.dropna(subset=['join_date'])
+        date_pattern = r'\b(\d{4}[-/]\d{2}[-/]\d{2}|\d{4} (?:January|February|March|April|May|June|July|August|September|October|November|December) \d{2}|\d{1,2} (?:January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2})\b'
+        print(users['date_of_birth'].loc[1023])
+        print(users['date_of_birth'].loc[867])
+        users['date_of_birth'] = users['date_of_birth'].str.extract(date_pattern, expand=False)
+        users = users[users['date_of_birth'].notnull()]
+        # users= users[~users['date_of_birth'].isna()]
+        print("ISNA" + users['date_of_birth'][users['date_of_birth'].isna()])
+        users['date_of_birth'] = pd.to_datetime(users['date_of_birth'], format="%Y-%m-%d", errors='coerce')
+        users['join_date'] = users['join_date'].str.extract(date_pattern, expand=False)
+        users = users[users['join_date'].notnull()]
+        users = users.dropna(subset='join_date')
+        users['join_date'] = pd.to_datetime(users['join_date'], format="%Y-%m-%d", errors='coerce')
         users = users.reset_index(drop=True)
         # Replacing GGB with GB - inferring
         users['country_code'] = users['country_code'].replace({'GGB':'GB'})
@@ -26,7 +33,21 @@ class DataCleaning:
         users = users[users['email_address'].str.contains('@')]
         users = users[users['email_address'].str.contains('.co.uk|.com|.de|.org|.info|.net|.biz')]
         users = users.reset_index(drop=True)
+        users.to_string('cleaned_users.txt')
         return users
-   
-    
+    def clean_card_data(self, df):
+        print(f"Total null values in the dataframe are : {df.isna().sum().sum()}")
+        print(df['card_provider'].unique())
+        df['card_provider'] = df['card_provider'].astype('category')
+        df['card_number'] =  df['card_number'].astype(int)
+        df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce')
+        df.to_string('clean_card.txt')
+        return df
+    def called_clean_store_date(self,df):
+        print(df['continent'].unique())
+        df['continent'] = df['continent'].replace({'eeAmerica':'America', 'eeEurope':'Europe'})
+        print(df['store_type'].unique())
 
+
+   
+ 
--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,8 @@
-# from database_utils import DatabaseConnector
 import pandas as pd
+import tabula
+import requests
+import json
+
 class DataExtractor:
     def __init__(self):
         pass
@@ -7,5 +10,34 @@ class DataExtractor:
             engine = connect.init_db_engine()
             users = pd.read_sql_query(f'''SELECT * FROM {table_name}''', engine).set_index('index')
             print(users.columns)
+            users.to_string('uncleaned_users.txt')
             return users
-    
+    def retrieve_pdf_data(self,link):
+        dfs = tabula.read_pdf(link,pages='all',stream='True')[0]
+        return dfs
+    def list_number_of_stores(self):
+         api_dict = {'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+         api_fetch = requests.get('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores',headers=api_dict)
+         api_json = api_fetch.json()
+         num_stores = api_json['number_stores']
+         return num_stores
+    def retrieve_stores_data(self,store_endpoint):
+         api_dict = {'x-api-key':'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+         api_fetch_list=[]
+         for i in range(num_stores):
+              api_fetch = requests.get(f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{i}', headers=api_dict)
+              api_fetch_list.append(api_fetch.json())
+         api_df = pd.DataFrame(api_fetch_list)
+         print(api_df)
+         return api_df
+        #  api_df = pd.json_normalize(api_json)
+        #  print(api_df)
+        #  return api_df
+obj_1 = DataExtractor()
+data  = obj_1.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+num_stores = obj_1.list_number_of_stores()
+stores_data = obj_1.retrieve_stores_data('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/')
+stores_data.to_string('cleaning_stores.txt')
+print(data)
+data.to_string('asdasd.txt')
+
